e0fcdb7a6b5b61218afdd0c0b3e6bc419c42e5e9
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2018-11-16 11:21:27 -0600

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -5,10 +5,6 @@ from discord.ext import commands
 import json
 import os
 
-import psycopg2
-
-DATABASE_URL = os.environ['https://data.heroku.com/datastores/413396c3-54c1-4519-8372-3284ff301284']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 bot = commands.Bot(command_prefix='#')
 
