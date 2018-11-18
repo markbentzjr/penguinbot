4ed608ec81d5c8ce8d7ed4bc20f38bbb7de9e2b0
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2018-11-17 18:29:53 -0600

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -22,12 +22,12 @@ try:
           (ID INT PRIMARY KEY     NOT NULL,
           MODEL           TEXT    NOT NULL,
           PRICE         REAL); '''
-        postgres_insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
-        record_to_insert = (5, 'One Plus 6', 950)
-        cursor.execute(postgres_insert_query, record_to_insert)
-   connection.commit()
-   count = cursor.rowcount
-   print (count, "Record inserted successfully into mobile table")
+    postgres_insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
+    record_to_insert = (5, 'One Plus 6', 950)
+    cursor.execute(postgres_insert_query, record_to_insert)
+    connection.commit()
+    count = cursor.rowcount
+    print (count, "Record inserted successfully into mobile table")
 except (Exception, psycopg2.DatabaseError) as error :
     print ("Error while creating PostgreSQL table", error)
 finally:
