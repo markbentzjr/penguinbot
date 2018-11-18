d7f97cd771ccaed5951cf08b9d7240bce8c891a7
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2018-11-18 17:41:27 -0600

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -21,7 +21,7 @@ cur.close()
 #        level INT);"""
 # cur.execute(r)
 # conn.commit()
-# sq1 = """ INSERT INTO users (user_id, experience, level) VALUES ('210653742133936128' 0 1)"""
+# sq1 = """ INSERT INTO users (user_id, experience, level) VALUES ('210653742133936128', 0, 1)"""
 # insert = (210653742133936128, 0, 1)
 # cur.execute(sq1)
 # conn.commit()
