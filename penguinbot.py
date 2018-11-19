6684e70cee0dee14eaef3150a3ecdb2ffbffb124
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2018-11-18 23:40:03 -0600

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -47,9 +47,9 @@ async def on_message(message):
     m = "{}".format(message.author.id)
     print("work1")
     cur = conn.cursor()
-    delt = """ DELETE FROM users WHERE user_id = %s; """
-    cur.execute(delt, (m,))
-    conn.commit()
+#    delt = """ DELETE FROM users WHERE user_id = %s; """
+#   cur.execute(delt, (m,))
+#    conn.commit()
     sq1 = """SELECT user_id FROM users; """
     cur.execute(sq1)
     n = cur.fetchall()
