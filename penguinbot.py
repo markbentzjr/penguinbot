959045edad6e0d1df715f01db8479cdb392a6f3e
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2018-11-18 23:10:09 -0600

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -54,7 +54,7 @@ async def on_message(message):
     if not message.author.id in n:
         sq2 = """ INSERT INTO users (user_id, experience, level) VALUES (%s, %s, %s)"""
         insert = (m, 5, 1)
-        cur.execute(sq2, (insert,))
+        cur.execute(sq2, (insert))
         conn.commit()
     getinfo = """SELECT experience FROM users WHERE user_id = %s; """
     print(m)
