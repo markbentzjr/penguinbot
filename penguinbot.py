8515d1dfaaf14d0c45b3b0cdee97bb0f15cecd12
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2018-11-21 01:53:43 -0600

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -117,7 +117,7 @@ async def leaderboard():
     updatesq4 = """ SELECT user_id FROM users ORDER BY experience DESC; """
     cur.execute(updatesq4)
     leader = cur.fetchmany(10)
-    lead = leader
+    lead = str(leader)
     print(lead)
     await bot.say("{}".format(server.get_member(lead)))
     cur.close()
