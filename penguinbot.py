52bdb40ebf01bace1684a96341503d58f662ad54
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2019-10-25 00:02:53 -0400

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -150,9 +150,9 @@ async def leaderboard(ctx):
     leader = cur.fetchmany(10)
     i = 0
     for i in range(0,10):
-        userexist = get(bot.get_all_members(), id=leader[i])
+        userexist = get(bot.get_all_members(), id=leader)
         print(userexist)
-        print(bot.get_all_members)
+        print(bot.get_all_members())
         i = i + 1
     await bot.say("{}".format(userexist))
     cur.close()
