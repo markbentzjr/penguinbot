23c885550f4a31a54256cb7fe63040fdd31d0084
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2020-09-30 19:09:44 -0500

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -13,7 +13,7 @@ scissors = 'scissors'
 paper = 'paper'
 DATABASE_URL = os.environ['DATABASE_URL']
 
-client = commands.Bot(command_prefix='#')
+bot = commands.Bot(command_prefix='#')
 
 
 @bot.event
@@ -23,8 +23,8 @@ async def on_ready():
 
 @bot.event
 async def on_message(message):
-    bot = message.channel
-    await client.process_commands(message)
+    botmsg = message.channel
+    await bot.process_commands(message)
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     m = "{}".format(message.author.id)
     print("work1")
@@ -54,7 +54,7 @@ async def on_message(message):
     print(lvl_start, lvl_end)
     if lvl_start[0] < lvl_end:
         lvl_end = lvl_start[0] + 1
-        await bot.send(message.channel, "{} has leveled up to level {}".format(message.author.mention, lvl_end))
+        await botmsg.send(message.channel, "{} has leveled up to level {}".format(message.author.mention, lvl_end))
         update_lvl = """ UPDATE users SET level = %s WHERE user_id = %s; """
         cur.execute(update_lvl, (lvl_end, m))
         conn.commit()
@@ -63,40 +63,40 @@ async def on_message(message):
         conn.close()
         print("PostgreSQL cursor is closed")
     if message.content == 'Penguin':
-        await bot.send(message.channel, ":penguin:")
+        await botmsg.send(message.channel, ":penguin:")
     if message.content == paper.upper().lower():
         rpsgame = random.choice(['Rock', 'Paper', 'Scissors'])
-        await bot.send(message.channel, "{}".format(rpsgame))
+        await botmsg.send(message.channel, "{}".format(rpsgame))
         if rpsgame == 'Scissors':
-            await bot.send(message.channel, "You Lose!")
+            await botmsg.send(message.channel, "You Lose!")
         elif rpsgame == 'Rock':
-            await bot.send(message.channel, "You Win!")
+            await botmsg.send(message.channel, "You Win!")
         elif rpsgame == 'Paper':
-            await bot.send(message.channel, "It's a Draw! Play Again?")
+            await botmsg.send(message.channel, "It's a Draw! Play Again?")
     if message.content == rock.upper().lower():
         rpsgame = random.choice(['Rock', 'Paper', 'Scissors'])
-        await bot.send(message.channel, "{}".format(rpsgame))
+        await botmsg.send(message.channel, "{}".format(rpsgame))
         if rpsgame == 'Paper':
-            await bot.send(message.channel, "You Lose!")
+            await botmsg.send(message.channel, "You Lose!")
         elif rpsgame == 'Scissors':
-            await bot.send(message.channel, "You Win!")
+            await botmsg.send(message.channel, "You Win!")
         elif rpsgame == 'Rock':
-            await bot.send(message.channel, "It's a Draw! Play Again?")
+            await botmsg.send(message.channel, "It's a Draw! Play Again?")
     if message.content == scissors.upper().lower():
         rpsgame = random.choice(['Rock', 'Paper', 'Scissors'])
-        await bot.send(message.channel, "{}".format(rpsgame))
+        await botmsg.send(message.channel, "{}".format(rpsgame))
         if rpsgame == 'Rock':
-            await bot.send(message.channel, "You Lose!")
+            await botmsg.send(message.channel, "You Lose!")
         elif rpsgame == 'Paper':
-            await bot.send(message.channel, "You Win!")
+            await botmsg.send(message.channel, "You Win!")
         elif rpsgame == 'Scissors':
-            await bot.send(message.channel, "It's a Draw! Play Again?")
+            await botmsg.send(message.channel, "It's a Draw! Play Again?")
             
             
 @bot.command(pass_context=True)
 async def ping(message):
-    bot = message.channel
-    await bot.send('pong')
+    botmsg = message.channel
+    await botmsg.send('pong')
 
 @bot.command(pass_context=True)
 async def join(ctx):
@@ -112,17 +112,17 @@ async def join(ctx):
 
 @bot.command(pass_context=True)
 async def embed(ctx):
-    bot = ctx.channel
+    botmsg = ctx.channel
     embed = discord.Embed(title="test", description="penguins", color=0x0000ff)
     embed.set_footer(text="Testing")
     embed.set_author(name="SteelPenguin87")
     embed.add_field(name="Field 1", value="okay", inline=True)
-    await bot.send(embed=embed)
+    await botmsg.send(embed=embed)
 
 
 @bot.command(pass_context=True)
 async def profile(ctx, user: discord.Member):
-    bot = ctx.channel
+    botmsg = ctx.channel
     embed = discord.Embed(title="{}'s info".format(user.name), description="You can't hide m8", color=0x0000ff)
     embed.add_field(name="Name", value=user.name, inline=True)
     embed.add_field(name="ID", value=user.id, inline=True)
@@ -130,25 +130,25 @@ async def profile(ctx, user: discord.Member):
     embed.add_field(name="Role", value=user.top_role, inline=True)
     embed.add_field(name="Joined", value=user.joined_at, inline=True)
     embed.set_thumbnail(url=user.avatar_url)
-    await bot.send(embed=embed)
+    await botmsg.send(embed=embed)
 
 
 @bot.command(pass_context=True)
 async def rank(ctx):
-    bot = ctx.channel
+    botmsg = ctx.channel
     m = "{}".format(ctx.message.author.id)
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
     updatesq3 = """ SELECT level FROM users WHERE user_id = %s; """
     cur.execute(updatesq3, (m,))
     rank = cur.fetchone()
-    await bot.send("{} you are rank {}!".format(ctx.message.author.mention, rank[0]))
+    await botmsg.send("{} you are rank {}!".format(ctx.message.author.mention, rank[0]))
     cur.close()
     conn.close()
 
 @bot.command(pass_context=True)
 async def leaderboard(ctx):
-    bot = ctx.channel
+    botmsg = ctx.channel
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
     cur = conn.cursor()
     updatesq4 = """ SELECT user_id FROM users ORDER BY experience DESC; """
@@ -156,11 +156,11 @@ async def leaderboard(ctx):
     leader = cur.fetchmany(10)
     i = 0
     for i in range(0,10):
-        userexist = get(bot.get_user(), id=leader[i])
+        userexist = get(bot.get_user(ctx), id=leader[i])
         print(userexist)
         print(bot.get_user(leader[i]))
         i = i + 1
-    await bot.send("{}".format(userexist))
+    await botmsg.send("{}".format(userexist))
     cur.close()
     conn.close()
 
