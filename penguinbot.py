3295a29077e713ede49806db3f27c60ef0f2514c
markbentzjr <45038138+markbentzjr@users.noreply.github.com>
2020-09-30 21:23:27 -0500

    Update penguinbot.py

--- penguinbot.py
+++ penguinbot.py
@@ -169,6 +169,8 @@ async def leaderboard(ctx):
         print(user_exist, bot.get_user(user_exist))
         if user_exist is not None:
             save = bot.get_user(user_exist)
+            name = save.name
+            print(save.name)
             leader_users.append(save)
             print(leader_users)
     await botmsg.send("{}".format(leader_users))
